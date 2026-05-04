ef26f3d3af62e6985898d4d420948e5375b936f2
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-05-04 09:01:28 +0200

    feat(lib): compose Python package sets

---
+++ lib/python-set.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  pyproject-nix,
+  pyproject-build-systems,
+}:
+
+let
+  errors = import ./errors.nix { };
+
+  pythonInterpreters = import ./interpreter.nix {
+    inherit lib;
+    fail = errors.fail;
+    show = errors.show;
+    pep440 = pyproject-nix.lib.pep440;
+  };
+
+  overlayNames = builtins.attrNames pyproject-build-systems.overlays;
+in
+{
+  build =
+    {
+      where,
+      pkgs,
+      interpreter ? null,
+      requiresPythonSource,
+      sourcePreference,
+      overlays ? [ ],
+      environ ? { },
+      stdenv ? pkgs.stdenv,
+      mkOverlay,
+    }:
+    let
+      checkedOverlays =
+        if builtins.isList overlays then overlays else errors.fail where "overlays must be a list";
+
+      resolvedInterpreter =
+        if interpreter == null then pythonInterpreters.choose pkgs requiresPythonSource else interpreter;
+
+      buildOverlay =
+        pyproject-build-systems.overlays.${sourcePreference}
+          or (errors.fail where "unknown sourcePreference ${sourcePreference}; available: ${lib.concatStringsSep ", " overlayNames}");
+
+      baseSet = pkgs.callPackage pyproject-nix.build.packages {
+        python = resolvedInterpreter;
+        inherit stdenv;
+      };
+
+      generatedOverlay = mkOverlay {
+        inherit sourcePreference environ;
+      };
+
+      pythonSet = baseSet.overrideScope (
+        lib.composeManyExtensions (
+          [
+            buildOverlay
+            generatedOverlay
+          ]
+          ++ checkedOverlays
+        )
+      );
+    in
+    {
+      inherit
+        checkedOverlays
+        resolvedInterpreter
+        pythonSet
+        ;
+    };
+}
