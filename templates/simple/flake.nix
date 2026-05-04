2525f967a85bddfc1dfb2ed556273c710a227553
Mattias Matthiesen <mattias.matthiesen@eviny.no>
2026-05-04 09:01:29 +0200

    feat(templates): provide simple package starter

---
+++ templates/simple/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "uvloom simple application template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    uvloom.url = "github:fornybar/uvloom";
+    uvloom.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, uvloom, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      project = uvloom.lib.loadProject { root = ./.; };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          scope = project.forPython {
+            inherit pkgs;
+            interpreter = pkgs.python312;
+          };
+        in
+        {
+          default = scope.mkApplication { package = "smiley-plot"; };
+        }
+      );
+    };
+}
